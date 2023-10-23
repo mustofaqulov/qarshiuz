4e5f1b1dfb6b3323cea52fe514ebf4632545b667
Ali Odiljonov <jahcoding@gmail.com>
2023-10-23 12:26:14 +0500

    refactor: remove unuseful codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,3 @@
-import { Button } from './components/index';
-
 export default function App() {
-  return (
-    <>
-      <h1>Hello Qarshi</h1>
-      <Button type="submit"></Button>
-    </>
-  );
+  return <h1>Hello Qarshi</h1>;
 }
