6a9325e4d1db48348e1c47308cd68fbc017eec1e
shaxzod <shokirovshaxozd32@gmail.com>
2023-10-30 10:28:11 +0500

    feat: past new config svgr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
