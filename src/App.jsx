16170646b943f91ae593c3aadbb33468f2685b69
Nurmamatov Ozodbek <nurmamatovozodbek971@gmail.com>
2023-10-30 15:21:50 +0500

    feat: edit file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import {Tab} from "./components/LanguageTab/Tab.jsx";
-import AddIcon from './assets/icons/add-icon.svg?react'
+import { Tab } from './components/LanguageTab/Tab';
+import AddIcon from './assets/icons/add-icon.svg?react';
 export default function App() {
-  return<>
-  <AddIcon fill={"#fff"}/>
-  <Tab/>
-  </>
+  return (
+    <>
+      <AddIcon fill={'#fff'} />
+      <Tab />
+    </>
+  );
 }
