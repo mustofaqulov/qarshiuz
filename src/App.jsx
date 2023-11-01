eff27a00951a62c5125242c7adcaf1da2e8b7e9b
Xasan <xasanasqarov15@gmail.com>
2023-11-01 16:42:46 +0500

    feat: create input

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Tab } from './components/LanguageTab/Tab';
-import AddIcon from './assets/icons/add-icon.svg?react';
+import AddIcon from './components/MiniCategory/MiniCards';
+import Input from './components/Input/Input';
 
 export default function App() {
   return (
     <>
       <AddIcon fill="#fff" />
       <Tab />
+      <Input />
     </>
   );
 }
