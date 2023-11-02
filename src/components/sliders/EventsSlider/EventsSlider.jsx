d3f91d21ca88c744c69f4287b079703193a61a19
Nurmamatov Ozodbek <nurmamatovozodbek971@gmail.com>
2023-11-02 18:26:00 +0500

    feat: add sliders components

---
+++ src/components/sliders/EventsSlider/EventsSlider.jsx
@@ -0,0 +1,38 @@
+import {
+  Swiper,
+  SwiperSlide,
+} from 'swiper/react';
+import 'swiper/css';
+import classNames from 'classnames';
+import { SliderButtons } from '../SliderButtons/SliderButtons';
+import EventsCard from '../../cards/EventsCard/EventsCard';
+import style from '../sliders.module.scss';
+
+export default function EventsSlider() {
+  return (
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      breakpoints={{
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+      }}
+      speed={1000}
+      className={classNames(style.sliders)}
+    >
+      <SwiperSlide>
+        <EventsCard />
+      </SwiperSlide>
+      <SliderButtons />
+    </Swiper>
+  );
+}
