09438cc10d5af776fcee2deebe5715fcf74a6b48
Islombek <mustofaqulov079@gmail.com>
2023-11-05 00:13:59 +0500

    category

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Layout } from './layout/Layout';
 import { HomePage } from './pages';
+import { Category } from './pages/Category/Category';
 
 export default function App() {
   return <Category />;
