64fe4b4086ecf70a19e516e1a3a24116ab10ae21
Islombek <mustofaqulov079@gmail.com>
2023-11-04 22:55:37 +0500

    new button still

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { Layout } from './layout/Layout';
 import { HomePage } from './pages/HomePage/HomePage';
-import { Select } from './components/Select/Select';
 
 export default function App() {
   return (
     <Layout>
-      {/* <HomePage /> */}
-      <Select />
+      <HomePage />
     </Layout>
   );
 }
