b85d3e0f528ef64b345c2dc46ae49289e929895b
Xasan <xasanasqarov15@gmail.com>
2023-11-05 04:07:02 +0500

    creat page  404

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,5 +1,8 @@
 import classNames from 'classnames';
+import { Input } from '../../components';
+import { Button } from '../../components';
 import style from './category.module.scss';
+// import MenuIcon from '../../assets/icons/menu-icon.svg';
 
 export function Category() {
   return (
@@ -9,7 +12,33 @@ export function Category() {
           style['category-info'],
         )}
       >
-        <h1>hello</h1>
+        <h1>
+          Discover the diversity of our city
+        </h1>
+        <p>
+          Discover the diversity of our city,
+          where history, culture, nature and
+          entertainment combine in a unique way.
+        </p>
+      </div>
+      <div
+        className={classNames(
+          style['category-search'],
+        )}
+      >
+        <button
+          className={classNames(
+            style['filter-button'],
+          )}
+        >
+          {/* <MenuIcon /> */}
+          <span>Categorys</span>
+        </button>
+        <Input placeholder="Events" />
+        <Button
+          title="Search"
+          btnClass="primary"
+        />
       </div>
     </div>
   );
