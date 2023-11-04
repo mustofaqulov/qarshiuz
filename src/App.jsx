5deea61c3b4a48456191e1b244cca4172100a0fc
Islombek <mustofaqulov079@gmail.com>
2023-11-04 16:37:08 +0500

    create select component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Layout } from './layout/Layout';
 import { HomePage } from './pages/HomePage/HomePage';
+import { Select } from './components/Select/Select';
 
 export default function App() {
   return (
     <Layout>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Select />
     </Layout>
   );
 }
