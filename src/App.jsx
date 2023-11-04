0a885d3303396a4d6ddf734670f835cdc0e9d8da
Islombek <mustofaqulov079@gmail.com>
2023-11-05 00:19:54 +0500

    create select component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Layout } from './layout/Layout';
 import { HomePage } from './pages';
-import { Category } from './pages/Category/Category';
 
 export default function App() {
-  return <Category />;
+  return (
+    <Layout>
+      <HomePage />
+    </Layout>
+  );
 }
