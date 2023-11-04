c92079d364103166e69d231fad5a92996ebbeee7
Xasan <xasanasqarov15@gmail.com>
2023-11-05 01:14:21 +0500

    reafactor: new pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import { MiniCards } from './components';
 import { Layout } from './layout/Layout';
 import { HomePage } from './pages';
-// import MiniCards from './components/MiniCards/MiniCards';
-// import { MiniCards } from './components';
-// import { MiniComponents } from './components';
 
 export default function App() {
   return (
