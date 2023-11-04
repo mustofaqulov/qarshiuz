5062a518c6fef8a0f69949cd209f02fc94d94632
Islombek <mustofaqulov079@gmail.com>
2023-11-05 04:20:56 +0500

    category

--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,10 +1,51 @@
 import classNames from 'classnames';
-import { Input } from '../../components';
-import { Button } from '../../components';
+import { useState } from 'react';
+import { Input } from '../../components/Input/Input';
+import { Button } from '../../components/Button/Button';
+import { CategoryCard } from '../../components/Cards/CategoryCard/CategoryCard';
+// import Sele
+import CategoryMenuIcon from '../../assets/icons/categorys-menu.svg';
+import ClouseIcon from '../../assets/icons/clouse-icon-white.svg';
+import FilterIcon from '../../assets/icons/filter-white.svg';
 import style from './category.module.scss';
-// import MenuIcon from '../../assets/icons/menu-icon.svg';
+
+const btnData = [
+  {
+    id: 1,
+    title: 'Restaurant',
+  },
+  {
+    id: 2,
+    title: 'Hotel',
+  },
+  {
+    id: 3,
+    title: 'Sport club',
+  },
+  {
+    id: 4,
+    title: 'Sport club',
+  },
+  {
+    id: 5,
+    title: 'Bar',
+  },
+  {
+    id: 6,
+    title: 'Library',
+  },
+  {
+    id: 7,
+    title: 'Banks',
+  },
+  {
+    id: 8,
+    title: 'Sport club',
+  },
+];
 
 export function Category() {
+  const [close, setClose] = useState(false);
   return (
     <div className={classNames(style.category)}>
       <div
@@ -30,8 +71,13 @@ export function Category() {
           className={classNames(
             style['filter-button'],
           )}
+          onClick={() => setClose(!close)}
         >
-          {/* <MenuIcon /> */}
+          {close ? (
+            <ClouseIcon />
+          ) : (
+            <CategoryMenuIcon />
+          )}
           <span>Categorys</span>
         </button>
         <Input placeholder="Events" />
@@ -39,6 +85,56 @@ export function Category() {
           title="Search"
           btnClass="primary"
         />
+      </div>
+      <div
+        className={classNames(
+          style['category-fiter-btn'],
+        )}
+      >
+        <div className={classNames(style.btns)}>
+          <Button
+            title="Filter"
+            btnClass="primary"
+            icon={<FilterIcon />}
+          />
+          {btnData.map((btn) => (
+            <Button
+              key={btn.id}
+              title={btn.title}
+              btnClass="secondary"
+            />
+          ))}
+        </div>
+      </div>
+      <div className={classNames(style.wrapper)}>
+        <div
+          className={classNames(style.results)}
+        >
+          <div
+            className={classNames(style.result)}
+          >
+            <h3>Result</h3>
+            <span>45</span>
+          </div>
+
+        </div>
+        <div className={classNames(style.cards)}>
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+          <CategoryCard />
+        </div>
       </div>
     </div>
   );
