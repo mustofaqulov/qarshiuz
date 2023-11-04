8366e53d11f94ea4145e34c041ea260f235ca0aa
Islombek <mustofaqulov079@gmail.com>
2023-11-04 23:16:45 +0500

    create category

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { Layout } from './layout/Layout';
-import { HomePage } from './pages/HomePage/HomePage';
+// import { Layout } from './layout/Layout';
+// import { HomePage } from './pages/HomePage/HomePage';
+import { Category } from './pages/Category/Category';
 
 export default function App() {
-  return (
-    <Layout>
-      <HomePage />
-    </Layout>
-  );
+  return <Category />;
 }
