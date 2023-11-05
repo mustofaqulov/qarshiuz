bcae6773380bed249af48207e7819bfdfe7d9caa
shaxzod <shokirovshaxozd32@gmail.com>
2023-11-05 20:50:09 +0500

    refactor: pull new changes

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,20 +1,38 @@
 import {
-  BrowserRouter,
+  // BrowserRouter,
   Route,
   Routes,
 } from 'react-router-dom';
+import App from '../App';
 import { routePaths } from '../constants/routers';
-import { HomePage } from '../pages';
+import {
+  HomePage,
+  Category,
+  EventsPage,
+} from '../pages';
+import Notfound from '../errors/404';
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
+    <Routes>
+      <Route
+        path={routePaths.HOME}
+        element={<App />}
+      >
+        <Route index element={<HomePage />} />
         <Route
-          path={routePaths.HOME}
-          element={<HomePage />}
+          path={routePaths.CATEGORY}
+          element={<Category />}
         />
-      </Routes>
-    </BrowserRouter>
+        <Route
+          path={routePaths.EVENTS}
+          element={<EventsPage />}
+        />
+        <Route
+          path={routePaths.NOT_FOUND}
+          element={<Notfound />}
+        />
+      </Route>
+    </Routes>
   );
 }
