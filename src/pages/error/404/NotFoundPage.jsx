fc7f9ad4e2271accaacfa5f80053552b06918303
Xasan <xasanasqarov15@gmail.com>
2023-11-11 21:07:21 +0500

    fix: change some tags in NotFoundPage

--- src/pages/error/404/NotFoundPage.jsx
+++ src/pages/error/404/NotFoundPage.jsx
@@ -6,9 +6,9 @@ export function NotFoundPage() {
   return (
     <div className={style['notfound-blog']}>
       <h1>404</h1>
-      <p className={style['notfound-title']}>
+      <h2 className={style['notfound-title']}>
         Oops, Sorry we can &#39;t find that page!
-      </p>
+      </h2>
       <p
         className={style['notfound-description']}
       >
