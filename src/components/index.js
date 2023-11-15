f59acaf389c3a1f4677c7163146ad94737c4c7e5
shaxzod <shokirovshaxozd32@gmail.com>
2023-11-15 11:52:27 +0500

    fix: delete Navbar folder

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ export { CategoryCard } from './Cards/CategoryCard/CategoryCard';
 export { EventsSlider } from './Sliders/EventsSlider/EventsSlider';
 export { Footer } from './Footer/Footer';
 export { Input } from './Input/Input';
-export { Navbar } from './Navbar/Navbar';
 export { NewsSlider } from './Sliders/NewsSlider/NewsSlider';
 export { Pagination } from './Pagination/Pagination';
 export { PromoSlider } from './PromoSlider/PromoSlider';
