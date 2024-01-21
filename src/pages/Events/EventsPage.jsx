6dafd5da09833786ec3103b04301a9f6da847425
Xushmuratov Ibrohim <xushmurodovibrohim3@gmail.com>
2024-01-21 14:59:16 +0500

    fix: fix scrolling

--- src/pages/Events/EventsPage.jsx
+++ src/pages/Events/EventsPage.jsx
@@ -1,46 +1,50 @@
 import classNames from 'classnames';
+import { useEffect } from 'react';
 import style from './events-page.module.scss';
 import {
-  EventsCard,
-  Pagination,
-  Select,
+   EventsCard,
+   Pagination,
+   Select,
 } from '../../components';
 import { eventsCardData } from '../../utils/mock';
 
 export function EventsPage() {
-  return (
-    <div
-      className={classNames(style['events-page'])}
-    >
-      <section
-        className={classNames(
-          style['promo-section'],
-          style.container,
-        )}
+   useEffect(() => {
+      scroll(0, -100)
+   }, [])
+   return (
+      <div
+         className={classNames(style['events-page'])}
       >
-        <h1>don&rsquo;t miss city events!</h1>
-        <p>
-          Discover the diversity of our city,
-          where history, culture, nature and
-          entertainment combine in a unique way.
-        </p>
-      </section>
-      <section
-        className={classNames(
-          style['cards-sorted'],
-          style.container,
-        )}
-      >
-        <div className={classNames(style.sorted)}>
-          <Select />
-        </div>
-        <Pagination
-          component={<EventsCard />}
-          componentData={eventsCardData}
-        />
-        {/* <div className={classNames(style.cards)}>
+         <section
+            className={classNames(
+               style['promo-section'],
+               style.container,
+            )}
+         >
+            <h1>don&rsquo;t miss city events!</h1>
+            <p>
+               Discover the diversity of our city,
+               where history, culture, nature and
+               entertainment combine in a unique way.
+            </p>
+         </section>
+         <section
+            className={classNames(
+               style['cards-sorted'],
+               style.container,
+            )}
+         >
+            <div className={classNames(style.sorted)}>
+               <Select />
+            </div>
+            <Pagination
+               component={<EventsCard />}
+               componentData={eventsCardData}
+            />
+            {/* <div className={classNames(style.cards)}>
         </div> */}
-      </section>
-    </div>
-  );
+         </section>
+      </div>
+   );
 }
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,108 +1,112 @@
 import classNames from 'classnames';
+import { useEffect } from 'react';
 import {
-  Button,
-  SearchInput,
-  PromoSlider,
-  MiniCategory,
-  sliderData,
-  SubTag,
+   Button,
+   SearchInput,
+   PromoSlider,
+   MiniCategory,
+   sliderData,
+   SubTag,
 } from '../../components';
 import style from './home-page.module.scss';
 import { smallBtnData } from '../../utils/mock';
 
 export function HomePage() {
-  return (
-    <div
-      className={classNames(style['home-page'])}
-    >
-      <section
-        className={classNames(
-          style['promo-section'],
-        )}
+   useEffect(() => {
+      scroll(0, -100)
+   }, [])
+   return (
+      <div
+         className={classNames(style['home-page'])}
       >
-        <div
-          className={classNames(
-            style['promo-about'],
-            style.container,
-          )}
-        >
-          <h1>
-            Information portal of Qarshi city
-          </h1>
-          <div
-            className={classNames(style.about)}
-          >
-            <p>
-              A portal that helps you find places
-              in the city. You can get information
-              about pharmacies, hospitals,
-              restaurants and cafes
-            </p>
-            <div
-              className={classNames(
-                style['promo-search'],
-              )}
-            >
-              <div
-                className={classNames(
-                  style['input-button'],
-                )}
-              >
-                <SearchInput placeholder="Events" />
-                <Button
-                  btnClass="primary"
-                  title="search"
-                />
-              </div>
-              <div
-                className={classNames(
-                  style['promo-category'],
-                )}
-              >
-                {smallBtnData.map(
-                  ({ title, id }) => {
-                    return (
-                      <SubTag
-                        key={id}
-                        tagClass="sub-tag"
-                        title={title}
-                      />
-                    );
-                  },
-                )}
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className={classNames(
-            style['promo-slider'],
-            style.container,
-          )}
-        >
-          <PromoSlider />
-        </div>
-      </section>
-      <section className="container">
-        <MiniCategory />
-      </section>
-      {sliderData.map((slider) => {
-        const { id, title, component } = slider;
-        return (
-          <section
-            key={id}
+         <section
             className={classNames(
-              style['slider-section'],
-              style.container,
+               style['promo-section'],
             )}
-          >
-            <h3>{title}</h3>
-            <div className="slide">
-              {component}
+         >
+            <div
+               className={classNames(
+                  style['promo-about'],
+                  style.container,
+               )}
+            >
+               <h1>
+                  Information portal of Qarshi city
+               </h1>
+               <div
+                  className={classNames(style.about)}
+               >
+                  <p>
+                     A portal that helps you find places
+                     in the city. You can get information
+                     about pharmacies, hospitals,
+                     restaurants and cafes
+                  </p>
+                  <div
+                     className={classNames(
+                        style['promo-search'],
+                     )}
+                  >
+                     <div
+                        className={classNames(
+                           style['input-button'],
+                        )}
+                     >
+                        <SearchInput placeholder="Events" />
+                        <Button
+                           btnClass="primary"
+                           title="search"
+                        />
+                     </div>
+                     <div
+                        className={classNames(
+                           style['promo-category'],
+                        )}
+                     >
+                        {smallBtnData.map(
+                           ({ title, id }) => {
+                              return (
+                                 <SubTag
+                                    key={id}
+                                    tagClass="sub-tag"
+                                    title={title}
+                                 />
+                              );
+                           },
+                        )}
+                     </div>
+                  </div>
+               </div>
             </div>
-          </section>
-        );
-      })}
-    </div>
-  );
+            <div
+               className={classNames(
+                  style['promo-slider'],
+                  style.container,
+               )}
+            >
+               <PromoSlider />
+            </div>
+         </section>
+         <section className="container">
+            <MiniCategory />
+         </section>
+         {sliderData.map((slider) => {
+            const { id, title, component } = slider;
+            return (
+               <section
+                  key={id}
+                  className={classNames(
+                     style['slider-section'],
+                     style.container,
+                  )}
+               >
+                  <h3>{title}</h3>
+                  <div className="slide">
+                     {component}
+                  </div>
+               </section>
+            );
+         })}
+      </div>
+   );
 }
